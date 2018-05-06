ba3c369b585d70f272985f6b2d79f9a5e4dfa7f0
mirko314 <mirko.budszuhn@rub.de>
2018-05-06 20:11:56 +0200

    initial commit

---
+++ lib/adminlte3/rails.rb
@@ -0,0 +1,8 @@
+require "adminlte3/rails/version"
+
+module Adminlte3
+  module Rails
+    class Engine < ::Rails::Engine
+    end
+   end
+end
---
+++ lib/adminlte3/rails/version.rb
@@ -0,0 +1,5 @@
+module Adminlte3
+  module Rails
+    VERSION = "0.1.0"
+  end
+end
